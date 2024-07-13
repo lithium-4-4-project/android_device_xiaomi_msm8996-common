b7b2d9e95ce9dd8d10fdcdd4b54320769f68d091
Yumi Yukimura <me.cafebabe@gmail.com>
2024-07-13 23:22:02 +0530

    msm8996-common: Switch to QTI Memtrack AIDL HAL
    Change-Id: Ibcbaff02fd56fbccc3a88c01a5f46a89cb212ef5

--- msm8996.mk
+++ msm8996.mk
@@ -196,11 +196,8 @@ PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@2.0-service \
     android.hardware.graphics.composer@2.1-service \
     android.hardware.graphics.mapper@2.0-impl-2.1 \
-    android.hardware.memtrack@1.0-impl \
-    android.hardware.memtrack@1.0-service \
     gralloc.msm8996 \
     hwcomposer.msm8996 \
-    memtrack.msm8996 \
     libdisplayconfig \
     libdisplayconfig.qti \
     libgralloc.qti \
@@ -208,7 +205,8 @@ PRODUCT_PACKAGES += \
     libtinyxml \
     vendor.display.config@1.0.vendor \
     vendor.display.config@2.0.vendor \
-    vendor.qti.hardware.display.mapper@2.0.vendor
+    vendor.qti.hardware.display.mapper@2.0.vendor \
+    vendor.qti.hardware.memtrack-service
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/calib.cfg:$(TARGET_COPY_OUT_VENDOR)/etc/hdr_config.cfg \
