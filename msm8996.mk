LineageOS/android_device_lge_msm8996-common

msm8996-common: Build missing libraries for 14 QPR3
Change-Id: I6a84526ca1e9f4e5140c8068646433cab3fe85b2

msm8996.mk
@@ -170,7 +170,8 @@ PRODUCT_PACKAGES += \
 # Gatekeeper
 PRODUCT_PACKAGES += \
     android.hardware.gatekeeper@1.0-impl \
-    android.hardware.gatekeeper@1.0-service
+    android.hardware.gatekeeper@1.0-service \
+    libion.vendor
 
 # Gesture Handler
 PRODUCT_PACKAGES += \
@@ -337,7 +338,9 @@ PRODUCT_PACKAGES += \
     libprotobuf-cpp-full \
     libqti_vndfwk_detect \
     librmnetctl \
-    android.hardware.radio@1.4-service.legacy
+    android.hardware.radio@1.4-service.legacy \
+    libsqlite.vendor \
+    libsysutils.vendor
 
 # Seccomp policy
 PRODUCT_COPY_FILES += \

lithium-4-4-project/android_device_xiaomi_msm8996-common

msm8996.mk
@@ -153,6 +153,7 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-service \
     camera.msm8996 \
     libgui_vendor \
+    libion.vendor \
     libstdc++_vendor \
     libui_shim.vendor \
     vendor.qti.hardware.camera.device@1.0
@@ -280,6 +281,7 @@ PRODUCT_PACKAGES += \
     android.hidl.base@1.0.vendor\
     android.hidl.manager@1.0 \
     android.hidl.memory@1.0.vendor \
+    libhidlmemory.vendor \
     libhidltransport \
     libhidltransport.vendor \
     libhwbinder \
@@ -400,7 +402,9 @@ PRODUCT_PACKAGES += \
     android.hardware.radio.config@1.2.vendor \
     android.hardware.radio.deprecated@1.0.vendor \
     android.hardware.secure_element@1.2.vendor \
+    libnetutils.vendor \
     librmnetctl \
+    libsqlite.vendor \
     libxml2
 
 PRODUCT_PACKAGES += \
@@ -412,7 +416,8 @@ PRODUCT_PACKAGES += \
 # Sensors
 PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-impl:64 \
-    android.hardware.sensors@1.0-service
+    android.hardware.sensors@1.0-service \
+    libpower.vendor
 
 # Shims
 PRODUCT_PACKAGES += \